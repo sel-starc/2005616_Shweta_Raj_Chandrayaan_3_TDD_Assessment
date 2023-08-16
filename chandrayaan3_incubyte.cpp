b5a4ec194037f42d16d1326a3729695cca41af79
sel-starc <90095060+sel-starc@users.noreply.github.com>
2023-08-16 13:33:06 +0530

    Add files via upload

--- chandrayaan3_incubyte.cpp
+++ chandrayaan3_incubyte.cpp
@@ -10,6 +10,13 @@ public:
     Spacecraft(int startX, int startY, int startZ, char startDir) : x(startX), y(startY), z(startZ), dir(startDir) {
         lastDir = startDir;
     }
+    void doCommand(char cmd) {
+        if (cmd == 'f' || cmd == 'b') {
+            move((cmd == 'f') ? 1 : -1);
+        } else {
+            turn(cmd);
+        }
+    }
     void move(int step) {
         if (dir == 'U') z += step;
         else if (dir == 'D') z -= step;
@@ -21,23 +28,23 @@ public:
             lastDir = dir;  
         }
     }
-   void turn(char cmd) {
-        if (dir == 'U' || dir == 'D') {
-            if (cmd == 'l') {
-                dir = leftTurn(lastDir);
+    void turn(char cmd) {
+                 if (dir == 'U' || dir == 'D') {
+                         if (cmd == 'l') {
+                            dir = leftTurn(lastDir);
                 lastDir = dir;
             } else if (cmd == 'r') {
-                dir = rightTurn(lastDir);
+                  dir = rightTurn(lastDir);
                 lastDir = dir;
             } else if (cmd == 'u') dir = 'U';
             else if (cmd == 'd') dir = 'D';
         } else {
             if (cmd == 'l') {
-                dir = leftTurn(dir);
+                              dir = leftTurn(dir);
                 lastDir = dir;
             } else if (cmd == 'r') {
                 dir = rightTurn(dir);
-                lastDir = dir;
+                           lastDir = dir;
             } else if (cmd == 'u') dir = 'U';
             else if (cmd == 'd') dir = 'D';
         }
@@ -57,11 +64,24 @@ public:
         return d;
     }
     void showPosition() {
-        cout << "Current Position: (" << x << ", " << y << ", " << z << ")\n";
-        cout << "Facing Direction: " << dir << "\n";
+                     cout << "Current Position: (" << x << ", " << y << ", " << z << ")\n";
+     cout << "Facing Direction: " << dir << "\n";
     }
 };
 int main() {
-    cout << "done\n";
+    int startX, startY, startZ;char startDir;
+                 string cmds;
+ cout << "Enter start x, y, z: ";
+ cin >> startX >> startY >> startZ;
+        cout << "Enter start direction: ";
+            cin >> startDir;
+        cout << "Enter command string: ";
+    cin >> cmds;
+             Spacecraft craft(startX, startY, startZ, startDir);
+    for (size_t i = 0; i<cmds.length(); i++) {
+                 craft.doCommand(cmds[i]);
+    }
+                  craft.showPosition();
     return 0;
 }
+
